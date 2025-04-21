9bc5aa635fb9adbcb0e134a2da26f15e3ec1b8f8
sumanbro0 <pandey.suman258@gmail.com>
2025-04-21 14:21:43 +0545

    refactor: remove GitHub stars fetching and related UI from sign-in and sign-up pages

--- src/features/auth/components/sign-in-view.tsx
+++ src/features/auth/components/sign-in-view.tsx
@@ -53,24 +53,6 @@ export default function SignInViewPage({ stars }: { stars: number }) {
       </div>
       <div className='flex h-full items-center justify-center p-4 lg:p-8'>
         <div className='flex w-full max-w-md flex-col items-center justify-center space-y-6'>
-          {/* github link  */}
-          <Link
-            className={cn('group inline-flex hover:text-yellow-200')}
-            target='_blank'
-            href={'https://github.com/kiranism/next-shadcn-dashboard-starter'}
-          >
-            <div className='flex items-center'>
-              <GitHubLogoIcon className='size-4' />
-              <span className='ml-1 inline'>Star on GitHub</span>{' '}
-            </div>
-            <div className='ml-2 flex items-center gap-1 text-sm md:flex'>
-              <IconStar
-                className='size-4 text-gray-500 transition-all duration-300 group-hover:text-yellow-300'
-                fill='currentColor'
-              />
-              <span className='font-display font-medium'>{stars}</span>
-            </div>
-          </Link>
           <ClerkSignInForm
             initialValues={{
               emailAddress: 'your_mail+clerk_test@example.com'
